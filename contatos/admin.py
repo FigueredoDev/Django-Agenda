c49bdc80917d923f531d7e4684abfe5924fc8465
Figueredo <jhowfigueredo2011@hotmail.com>
2022-08-11 13:55:08 -0300

    Django Admin
    - Adicionando usuário na sessão administrativa do Django.
    - Adicionado os Models de Contatos, Categoria na pagina Admin.

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categoria, Contato
+
 
 # Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'categoria')
+    list_filter = ('nome', 'categoria')  # Filtros
+    list_per_page = 10  # Itens por pagina
+    search_fields = ('nome', 'sobrenome', 'telefone')
+    list_display_links = ('nome',)
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -5,12 +5,18 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)  # blank = True - campo não obrigatorio (NULL)
     telefone = models.CharField(max_length=255)
     email = models.CharField(max_length=255, blank=True)
-    data_criado = models.DateTimeField(timezone.now)
+    data_criado = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):  # adicionar nome da classe no site
+        return self.nome
