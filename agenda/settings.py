e74c8278c86e1ac34b979fc9f886f3f640a1927d
Figueredo <jhowfigueredo2011@hotmail.com>
2022-08-11 16:38:34 -0300

    up

--- agenda/settings.py
+++ agenda/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['django.com']
 
