0c902477da901c5a88cf4270dd3f029f7b533dd8
Figueredo <jhowfigueredo2011@hotmail.com>
2022-08-11 09:26:54 -0300

    Insert models
    Adicionando tabelas ao banco de dados

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)  # blank = True - campo não obrigatorio (NULL)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criado = models.DateTimeField(timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
