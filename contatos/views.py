db339d57ae48082f8989ae7fce8b367d845ebb0d
Figueredo <jhowfigueredo2011@hotmail.com>
2022-08-12 15:26:24 -0300

    Pagination
    - Realizado a paginação dos itens da home page

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404
 from .models import Contato
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def index(request):
     contatos = Contato.objects.all()  # Coletando dados do banco
+    paginator = Paginator(contatos, 10)  # Show 10 contacts per page
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
