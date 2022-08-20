2ea8353c163c3c5c18ee9fe01c63f940cfd5ef0f
Figueredo <jhowfigueredo2011@hotmail.com>
2022-08-20 20:01:50 -0300

    Adicionado form de cadastro pelo site

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.core.validators import validate_email
 from django.contrib.auth.decorators import login_required
+from .models import FormContato
 
 
 # Create your views here.
@@ -81,4 +82,16 @@ def register(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != "POST":
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form = FormContato(request.POST, request.FILES)
+    if not form.is_valid():
+        messages.error(request, 'Erro ao enviar formulario!')
+        form = FormContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    messages.success(request, f'Contato {request.POST.get("nome")} salvo com sucesso!')
+    form.save()
+    return redirect('dashboard')
