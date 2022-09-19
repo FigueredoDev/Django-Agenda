f8d8ac8bd85d1a8f3eb1b39f4ff42ae1c5b9103a
Figueredo <jhowfigueredo2011@hotmail.com>
2022-09-19 13:51:37 -0300

    Unit Tests
    - Add Tests Contatos

--- contatos/tests.py
+++ contatos/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase  # noqa: F401
+from django.urls import reverse
 
 # Create your tests here.
+from .models import Categoria, Contato  # noqa: F401
+
+
+class ContatoModelsTest(TestCase):
+    def setUp(self) -> None:
+        self.contato = Contato(
+            nome="Jhonata", sobrenome="Figueredo", telefone="12334")
+        self.categoria = Categoria(nome="Amigos")
+
+    def test_valida_attr_nome(self):
+        self.assertEqual(self.contato.nome, "Jhonata")
+
+    def test_valida_attr_sobrenome(self):
+        self.assertEqual(self.contato.sobrenome, "Figueredo")
+
+    def test_valida_attr_nome_Categoria(self):
+        self.assertEqual(self.categoria.nome, "Amigos")
+
+
+class TestContatoURLs(TestCase):
+    def test_contato_url_index_esta_correta(self):
+        index_url = reverse('contatos:index')
+        self.assertEqual(index_url, '/')
+
+    def test_contato_url_busca_esta_correta(self):
+        busca_url = reverse('contatos:busca')
+        self.assertEqual(busca_url, '/busca/')
