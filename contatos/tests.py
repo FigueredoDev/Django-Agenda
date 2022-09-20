6d7e0c3e2a90b100bc8c94146b1d3f8af84aa617
Figueredo <jhowfigueredo2011@hotmail.com>
2022-09-20 13:30:48 -0300

    Update tests.py
    - Add test views

--- contatos/tests.py
+++ contatos/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase  # noqa: F401
-from django.urls import reverse
+from django.urls import resolve, reverse
 
+from . import views
 # Create your tests here.
 from .models import Categoria, Contato  # noqa: F401
 
@@ -22,10 +23,30 @@ class ContatoModelsTest(TestCase):
 
 
 class TestContatoURLs(TestCase):
-    def test_contato_url_index_esta_correta(self):
+    def test_contatos_url_index_is_correct(self):
         index_url = reverse('contatos:index')
         self.assertEqual(index_url, '/')
 
-    def test_contato_url_busca_esta_correta(self):
+    def test_contatos_url_busca_is_correct(self):
         busca_url = reverse('contatos:busca')
         self.assertEqual(busca_url, '/busca/')
+
+    def test_contatos_url_detalhes_is_correct(self):
+        detalhes_url = reverse('contatos:detalhes', kwargs={'contato_id': 1})
+        self.assertEqual(detalhes_url, '/1')
+
+
+class TestContatosView(TestCase):
+    def test_contatos_index_view_function_is_correct(self):
+        view_index = resolve(reverse('contatos:index'))
+        self.assertIs(view_index.func, views.index)
+
+    def test_contatos_busca_view_function_is_correct(self):
+        view_busca = resolve(reverse('contatos:busca'))
+        self.assertIs(view_busca.func, views.busca)
+
+    def test_contatos_detalhes_view_function_is_correct(self):
+        view_detalhes = resolve(reverse(
+            'contatos:detalhes', kwargs={'contato_id': 1})
+        )
+        self.assertIs(view_detalhes.func, views.detalhes)
